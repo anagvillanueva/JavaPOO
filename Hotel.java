8147b972d8cc0bde52109d0dec4f223400293477
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2024-01-01 23:10:10 -0600

    Sesion 1

---
+++ Hotel.java
@@ -0,0 +1,14 @@
+public class Hotel { // Clase
+
+    // Atributos
+    int id;
+    String name;
+    String address;
+    int stars;
+    int numberOfRooms;
+
+    // Métodos
+    public void showName(){
+        System.out.println(name);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    // Punto de entrada de mi aplicación
+    public static void main(String[] args) {
+        Hotel myHotel = new Hotel();
+        myHotel.name = "Holiday Inn";
+        myHotel.showName();
+    }
+}
