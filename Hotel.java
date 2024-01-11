1f5a561a536ca209ab0199b161fe08669d65f21d
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2024-01-10 22:17:49 -0600

    Sesion 2
    Metodo constructor, This, Variables y Metodos Estaticos

--- Hotel.java
+++ Hotel.java
@@ -1,14 +1,37 @@
 public class Hotel { // Clase
-
+    static int id;
     // Atributos
-    int id;
     String name;
     String address;
     int stars;
-    int numberOfRooms;
 
-    // Métodos
+    // Metodo constructor
+
+    Hotel(){
+        System.out.println("-- Construyendo Hotel --");
+        id++;
+    }
+
+    // Metodos
     public void showName(){
         System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("ID Hotel: " + id);
+    }
+
+    public Object showDetails(){
+        System.out.println("Nombre:" + this.name);
+        System.out.println("Dirección:" + this.address);
+        System.out.println("Estrellas:" + this.stars);
+        return null;
+    }
+
+    public String showDetails2(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Nombre:" + this.name);
+        sb.append("\nDirección:" + this.address);
+        sb.append("\nEstrellas:" + this.stars);
+        return sb.toString();
     }
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,30 @@
+
 public class Main {
     // Punto de entrada de mi aplicación
     public static void main(String[] args) {
-        Hotel myHotel = new Hotel();
+
+
+        Hotel myHotel = new Hotel(); // Metodo constructor
+        System.out.println(Hotel.id);
         myHotel.name = "Holiday Inn";
+        myHotel.stars = 4;
+        myHotel.address = "Dr. Mariano Azuela 12, Cuauhtémoc, CDMX,";
+
+        Hotel myHotelSay = new Hotel(); // Nuevo objeto
+        System.out.println(Hotel.id);
+
         myHotel.showName();
+        System.out.println("-------------------");
 
         // Accediendo de forma publica
         System.out.println("Estrellas:" + myHotel.stars);
+
+        System.out.println("-------------------");
+        //Imprimiendo
+        myHotel.showDetails();
+        System.out.println("-------------------");
+        //Retornando cadena
+        System.out.println(myHotel.showDetails2());
+
     }
 }
